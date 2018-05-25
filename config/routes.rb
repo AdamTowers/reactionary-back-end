70ed40052a2956520c2e1c07621c5289c15dc1a8
adamtowers <hello@adamtowers.design>
2018-05-25 09:58:09 -0400

    Added serializer

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  get '/users/:id', to: 'users#show'
+  get '/users', to: 'users#index'
+  post '/users', to: 'users#create'
+
 end
