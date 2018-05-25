d196719976e10e11ac43cc501b5de9045ab99f81
adamtowers <hello@adamtowers.design>
2018-05-25 10:31:50 -0400

    Initial JWT and auth

--- Gemfile
+++ Gemfile
@@ -21,6 +21,8 @@ gem 'bcrypt', '~> 3.1.7'
 
 gem 'active_model_serializers'
 
+gem "jwt", "~> 2.1"
+
 gem 'faker'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :users
+      resources :rooms
 
-
-  get '/users/:id', to: 'users#show'
-  get '/users', to: 'users#index'
-  post '/users', to: 'users#create'
-
+      post '/sessions/', to: 'sessions#create'
+    end
+  end
 end
