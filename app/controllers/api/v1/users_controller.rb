647ce7308f40b6a7a6b3166a23fcac78f798d408
adamtowers <hello@adamtowers.design>
2018-05-27 20:10:02 -0400

    Changed relationship between user and room in serializers.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,6 +8,20 @@ module Api
         render json: @users, status: 200
       end
 
+      def create
+        @user = User.new(username: params["username"], password: params["password"])
+
+        if @user.valid?
+          @user.save
+          render json: @user, status: 200
+        else
+          # not sure if we need flash[:errors] for this but if so:
+          # flash[:errors] = @user.errors.full_messsages
+          render json: @user
+          # status?
+        end
+      end
+
       def show
         @user = @user.find(params[:id])
 
